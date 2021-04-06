6cc8a8f9a95ebc167427bb2a4220ea6def091fcb
esra.cetintas@outlook.com.tr <esra.cetintas@outlook.com.tr>
2021-04-06 16:15:06 +0300

    6.04.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,7 +34,7 @@ namespace Business.Concrete
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
-           return new SuccessDataResult<List<Product>>( _productDal.GetAll(),Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>( _productDal.GetAll(),Messages.ProductsListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int categoryId)
